db7f06bae94362c8d0b1e85d8389f512fe881e21
Yashraj Chavda <yashrajpchavda@gmail.com>
2018-06-29 13:16:57 +0530

    Initial Commit - Added source code files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# weather-sync
-A repository to hold code of assignment
+# WeatherSync
+
+> Ridiculously simple CORS-enabled API proxy for IP Geolocation (`/ip`) and Weather lookup by lat/lng (`/weather/$lat,$lng`)
+
+Deployed to Heroku for use by candidates in their pre-interview code challenge.
---
+++ index.js
@@ -0,0 +1,59 @@
+var express = require('express'),
+	cors = require('cors'),
+	proxy = require('http-proxy-middleware'),
+	morgan = require('morgan');
+
+var WEATHER_KEY = process.env.WEATHER_KEY;
+
+var app = express();
+
+app.set('trust proxy', true);
+
+function getIp(req) {
+	var fwd = req.headers['x-forwarded-for'],
+		ip = req.ip;
+	if (fwd) {
+		ip = fwd.split(',').pop() || ip;
+	}
+	if (ip==='::1') ip = '0.0.0.0';
+	return ip;
+}
+
+// allow x-forwarded-for from Heroku
+app.use(function(req, res, next) {
+	req.ip = getIp(req);
+	next();
+});
+
+// log standard apache format to stderr
+app.use(morgan('combined'));
+
+// enable CORS
+app.use( cors({
+	maxAge: 1728000
+}) );
+
+// proxy to GeoIP API
+app.get('/ip', function(req, res, next) {
+	req.url = req.url.replace(/\/*$/g, '/' + encodeURIComponent(getIp(req)));
+	next();
+}, proxy('/ip', {
+	target: 'https://geoip.nekudo.com',
+	changeOrigin: true,
+	pathRewrite: {
+		'^/ip': '/api'
+	}
+}));
+
+// proxy to weather API
+app.get('/weather/:geo', proxy('/', {
+	target: 'http://api.openweathermap.org',
+	xfwd: true,
+	changeOrigin: true,
+	pathRewrite: {
+		'^/weather/([0-9.-]*?),([0-9.-]*?)/?$': '/data/2.5/weather?lat=$1&lon=$2&appid='+WEATHER_KEY
+	}
+}));
+
+// accept requests on PORT
+app.listen(process.env.PORT || 8080);
